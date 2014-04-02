fe77735ebc08c44c2e0650c564d41fadd2daa2da
Dan Rosenberg Osted <danosted@gmail.com>
2014-04-02 13:17:18 +0200

    Mono userprefs setup

--- UnityProject/Assets/CharacterGAMetrics.cs
+++ UnityProject/Assets/CharacterGAMetrics.cs
@@ -1,43 +1,51 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CharacterGAMetrics : MonoBehaviour {
+public class CharacterGAMetrics : MonoBehaviour
+{
 
 	[SerializeField]
-	private GazeThesisTest gazeHandler;
+	private GazeThesisTest
+		gazeHandler;
 	[SerializeField]
-	private float hitCheckDistance = 100f;
+	private float
+		hitCheckDistance = 100f;
 
 	private Camera characterCamera;
 	private Vector3 gazePos = Vector3.zero;
 	private Transform currentTarget;
 
-	void Start () 
+	void Start()
 	{
-		characterCamera = GetComponentInChildren<Camera> ();
-		StartCoroutine (CollectMetrics ());
-		StartCoroutine (GazeRay ());
+		characterCamera = GetComponentInChildren<Camera>();
+		StartCoroutine(CollectEyetrackerMetrics());
+		StartCoroutine(CollectMetrics());
+		StartCoroutine(GazeRay());
 	}
 
 	private IEnumerator GazeRay()
 	{
-		while (true) {
+		while(true)
+		{
 			gazePos = transform.position;
-			try {
-					gazePos = gazeHandler.GetGazeScreenPosition ();
-			} catch (System.Exception ex) {
-					Debug.Log (ex);
+			try
+			{
+				gazePos = gazeHandler.GetGazeScreenPosition();
 			}
-			gazePos = characterCamera.ScreenToWorldPoint (new Vector3 (gazePos.x, gazePos.y, characterCamera.nearClipPlane));
+			catch(System.Exception ex)
+			{
+				Debug.Log(ex);
+			}
+			gazePos = characterCamera.ScreenToWorldPoint(new Vector3(gazePos.x, gazePos.y, characterCamera.nearClipPlane));
 
 			//Ray showing gaze direction
-			Debug.DrawRay (characterCamera.transform.position, (gazePos - characterCamera.transform.position) * 20f);
+			Debug.DrawRay(characterCamera.transform.position, (gazePos - characterCamera.transform.position) * 20f);
 			RaycastHit hit;
 			Ray ray = new Ray(characterCamera.transform.position, (gazePos - characterCamera.transform.position));
 			if(Physics.Raycast(ray, out hit, hitCheckDistance))
 			{
 				currentTarget = hit.transform;
-				Debug.Log ("hit something: " + currentTarget.name,currentTarget.gameObject);
+				Debug.Log("hit something: " + currentTarget.name, currentTarget.gameObject);
 			}
 			else
 			{
@@ -51,6 +59,16 @@ public class CharacterGAMetrics : MonoBehaviour {
 
 	private IEnumerator CollectMetrics()
 	{
+		while(true)
+		{
+			GA.API.Design.NewEvent("RandomTestPosition", gazeHandler.GetMeanPupilDilation(), transform.position);
+			
+			yield return new WaitForSeconds(1f);
+		}
+	}
+
+	private IEnumerator CollectEyetrackerMetrics()
+	{
 //		Vector3 screen_coord = Input.mousePosition;
 //		Vector3 world_coord = Camera.main.ScreenToWorldPoint(new Vector3(screen_coord.x, screen_coord.y, transform.position.z - Camera.main.transform.position.z));
 //		gazeHandler.transform.position = world_coord;
@@ -58,7 +76,7 @@ public class CharacterGAMetrics : MonoBehaviour {
 //		float pos_x = world_coord.x;
 //		pos_x =  Mathf.Clamp(pos_x, -MaxVelocityChange, MaxVelocityChange);
 //		transform.position = new Vector3(pos_x, transform.position.y, transform.position.z);
-		while (true) 
+		while(gazeHandler)
 		{
 
 //			GA.API.Design.NewEvent("PupilDilationLeft", gazeHandler.GetPupilDilationLeft(), transform.position);
